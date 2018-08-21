8438b229f6c9661a7a941fcfc9ea29da4a0f7c69
diegodenzer <diegodenzer@hotmail.com>
2018-08-21 17:18:09 -0300

    requirements

--- jogo/models.py
+++ jogo/models.py
@@ -7,8 +7,8 @@ class Atleta(models.Model):
     apelido = models.CharField(max_length=50)
     data_nascimento = models.DateField(null=True)
     local_nascimento = models.CharField(max_length=50)
-    descricao = models.TextField()
     imagem = models.ImageField(upload_to='atletas', null=True, blank=True)
+    numero_camisa = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
         db_table = 'atleta'
@@ -18,13 +18,35 @@ class Atleta(models.Model):
     def __str__(self):
         return self.nome + " " + self.sobrenome
 
+class Adversario(models.Model):
+    nome = models.CharField(max_length=50)
+    escudo_adversario = models.ImageField(default="comuns/sem_escudo.png", upload_to='jogos', null=True, blank=True)
+
+    class Meta:
+        db_table = 'adversario'
+        verbose_name = "Adversário"
+        verbose_name_plural = "Adversários"
+
+    def __str__(self):
+        return self.nome
+
+class Local(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=150, null=True, blank=True)
+
+    class Meta:
+        db_table = 'local'
+        verbose_name = "Local"
+        verbose_name_plural = "Locais"
+
+    def __str__(self):
+        return self.nome
+
 
 class Jogo(models.Model):
-    adversario = models.CharField(max_length=50)
-    data = models.DateField()
+    adversario = models.ForeignKey(Adversario, on_delete=models.CASCADE)
+    data = models.DateTimeField()
     local = models.CharField(max_length=50)
-    arbitragem = models.CharField(max_length=80, null=True, blank=True)
-    escudo_adversario = models.ImageField(default="comuns/sem_escudo.png", upload_to='jogos', null=True, blank=True)
     placar_real = models.IntegerField(null=True, blank=True)
     placar_adversario = models.IntegerField(null=True, blank=True)
     resumo = models.TextField(null=True, blank=True)
@@ -41,3 +63,5 @@ class Jogo(models.Model):
 class JogoAtleta(models.Model):
     jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
     atleta = models.ForeignKey(Atleta,  on_delete=models.CASCADE)
+    gols = models.IntegerField(null=True, blank=True)
+    asssitencia = models.IntegerField(null=True, blank=True)
